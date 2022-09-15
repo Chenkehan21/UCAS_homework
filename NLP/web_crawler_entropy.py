0a884a3828a42dc2538bfebaad72ec54aacb75c8
Chenkehan21 <751722804@qq.com>
2022-09-15 22:19:21 +0800

    update

--- NLP/web_crawler_entropy.py
+++ NLP/web_crawler_entropy.py
@@ -1,20 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+from retrying import retry
 
 
+@retry(stop_max_attempt_number=10)
 def get_html(url):
     try:
         html = requests.get(url, timeout=3)
         html_code = html.text
         soup = BeautifulSoup(html_code, 'html.parser')
-        soup.find_all()
 
         return soup
     except:
         print("fail")
 
 
-def get_title_url_list(url):
+@retry(stop_max_attempt_number=10)
+def get_one_page_title_url_list(url):
     soup = get_html(url)
     div_list = soup.find_all(name='div', attrs="fmKVwHzYQ")
     title_list, url_list = [], []
@@ -27,8 +29,39 @@ def get_title_url_list(url):
     return title_url
 
 
-def get_book_section_list(url):
-    pass
+def get_all_title_url_list(root_url):
+    page_number = 1
+    page_url = root_url
+    print(page_url)
+    all_title_url = {}
+    new_page_title_url = get_one_page_title_url_list(page_url)
+    print("successfully get page %d"%page_number)
+    all_title_url.update(new_page_title_url)
+    while True:
+        last_page_title_url = new_page_title_url
+        page_number += 1
+        page_url = root_url + str(page_number) + '/'
+        new_page_title_url = get_one_page_title_url_list(page_url)
+        print("successfully get page %d"%page_number)
+        '''
+        Judge whether reach the final page.
+        method: if reach the final + 1 page, we will get the same thing. So we can 
+        check whether new page title urls are the same with last page title urls
+        '''
+        if all(x in last_page_title_url.keys() for x in new_page_title_url.keys()):
+            print("reach final page")
+            break
+        all_title_url.update(new_page_title_url)
+
+    return all_title_url, last_page_title_url, new_page_title_url
+
+
+def get_book_content_list(url):
+    title_url = get_title_url_list(url)
+    for book_title, url in title_url.items():
+        book_url = first_url + url
+        soup = get_html(book_url)
+        content_url = soup.find(name='div', attrs='lRSQphVsn').find_all('li')
 
 
 
@@ -52,8 +85,8 @@ def save_txt(root_url, first_url, save_path):
 
 
 if __name__ == "__main__":
-    root_url = ""
-    first_url = ""
+    root_url = "https://m.moyanxsw.com/quanben/sort/"
+    first_url = "https://m.moyanxsw.com/"
     save_path = ""
 
     save_txt(root_url, first_url, save_path)
